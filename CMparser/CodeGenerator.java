a8313a87e31776598a2b82e47941845ea668298c
Olivia Biancucci <obiancuc@uoguelph.ca>
2024-03-27 10:03:13 -0400

    created CodeGenerator.java

---
+++ CMparser/CodeGenerator.java
@@ -0,0 +1,87 @@
+import absyn.*;
+
+public class CodeGenerator implements AbsynVisitor{
+	public void visit( NameTy exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( SimpleVar exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( IndexVar exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( Vartype exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( NilExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( IntExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( BoolExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( VarExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( CallExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( OpExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( AssignExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( IfExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( WhileExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( ReturnExp exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( CompoundExp exp, int level, boolean isAddr){
+
+	}
+
+	public void visit( FuncDec exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( SimpleDec exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit( ArrayDec exp, int level, boolean isAddr){
+
+	}
+
+	public void visit( ExpList exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit ( DecLists exp, int level, boolean isAddr){
+
+	}
+  
+	public void visit ( VarDecLists exp, int level, boolean isAddr){
+
+	}
+}
