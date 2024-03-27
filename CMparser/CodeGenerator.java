282818418614cacb31711fdbf8b60e3413a716ef
Olivia Biancucci <Olivia Biancucci>
2024-03-27 16:42:21 -0400

    getting the main shell going

--- CMparser/CodeGenerator.java
+++ CMparser/CodeGenerator.java
@@ -19,6 +19,62 @@ public class CodeGenerator implements AbsynVisitor{
 	int pc = 7;
 
 	int globalOffset; //this will be subtracted by 1 for every int
+	int mainEntry = 0; //store where the main is
+
+	private void insert(NodeType node){
+        if(table.containsKey(node.name)){
+            table.get(node.name).add(0, node);
+        }
+        else{ 
+            ArrayList<NodeType> arrList = new ArrayList<NodeType>();
+            arrList.add(0, node);
+            table.put(node.name, arrList);
+        }
+    }
+
+    private ArrayList<NodeType> lookup(NodeType node){
+        if(table.containsKey(node.name)){
+            return table.get(node.name);
+        }
+        return null;
+    }
+
+    private void delete(int level){
+        boolean delCheck;
+        while(true)
+        {
+            delCheck = false;
+            for(ArrayList<NodeType> nodes : table.values())
+            {
+                if(nodes.isEmpty() == false)
+                {
+                    for(int i = 0; i < nodes.size(); i++)
+                    {
+                        NodeType node = nodes.get(i);
+                        if(node.level == level)
+                        {
+                            nodes.remove(i);
+                            if(nodes.size() == 0)
+                            {
+                                table.remove(node.name);
+                                delCheck = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if(delCheck == true) // if something is deleted
+                {
+                    break;
+                }
+            }
+            if(delCheck == false) // if nothing is deleted
+            {
+                break;
+            }
+        }
+    }
+
 
 	public void codeGenerator() {
 		int frameOffset = -2; //TODO: check this?
@@ -70,16 +126,53 @@ public class CodeGenerator implements AbsynVisitor{
 	}
 
 	//the "main function"
-	public void visit(){
+	public void visit(String fileName){
+		int inSavedLoc = 0;
+		int outSavedLoc = 0;
+		int inFuncLocation = 0;
+		int outFuncLocation = 0;
+
+		emitComment("C-Minus Compilation to TM Code");
+		emitComment("File: " + fileName);
 
 		//Prelude
-		emitComment("Standard Prelude:");
+		emitComment("Standard prelude:");
         emitRM("LD", gp, 0, ac, "load gp with maxaddress");
         emitRM("LDA", fp, 0, gp, "copy to gp to fp");
-        emitRM("ST", ac, 0, ac, "clear value at location 0");
+        emitRM("ST", ac, 0, ac, "clear location 0");
 		
 		//Jump to a different instruction
+		emitComment("Jump around i/o routines here");
+
+		//Input Routine
+		emitComment("code for input routine");
+		inSavedLoc = emitSkip(1);
+		inFuncLocation = emitSkip(0);
+		NodeType inNode = new NodeType("input", "VOID", 0);
+		insert(inNode);
 		emitRM("ST", ac, retFO, fp, "store return");
+		emitRM("IN", 0, 0, 0, "input");
+		emitRM("LD", 7, -1, 5, "return to caller");  //TODO: double check the argument values here
+
+		//Output Routine
+		emitComment("code for output routine");
+		outFuncLocation = emitSkip(0);
+		NodeType outNode = new NodeType("output", "-2", 0); //TODO: check -2 here
+		insert(outNode);
+		emitRM("ST", ac, retFO, fp, "store return");
+		emitRM("LD", 0, initOF, fp, "load output value");
+		emitRM("OUT", 0, 0, 0, "output");
+		emitRM("LD", 7, -1, 5, "return to caller");  //TODO: double check the argument values here
+		outSavedLoc = emitSkip(0);
+		emitBackup(outFuncLocation);
+		emitRM_Abs("LDA", pc, outSavedLoc, "jump around i/o code");
+        
+		emitRestore();
+        emitComment("End of standard prelude.");
+
+		if (mainEntry == 0){ //if there is no main function
+			emitRO("HALT", 0, 0, 0, ""); //emit error
+		}
 
 		//Finale
 		emitRM( "ST", fp, globalOffset + ofpFO, fp, "push ofp" );
@@ -88,7 +181,7 @@ public class CodeGenerator implements AbsynVisitor{
 		emitRM_Abs( "LDA", pc, mainEntry, "jump to main loc" );
 		emitRM( "LD", fp, ofpFO, fp, "pop frame" );
 
-		//TODO: end of execution comment?
+		emitComment("End of execution.");
 		emitRO( "HALT", 0, 0, 0, "" );
 	}
 
